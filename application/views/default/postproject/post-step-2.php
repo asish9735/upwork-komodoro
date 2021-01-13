e0338dfd9bd89c998a2dcf8c31287b1bd5f82287
originatesoft-mac3 <55700698+originatesoft-mac3@users.noreply.github.com>
2021-01-13 13:20:02 +0530

    Update post-step-2.php

--- application/views/default/postproject/post-step-2.php
+++ application/views/default/postproject/post-step-2.php
@@ -2,16 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //dd($memberInfo,TRUE);
 ?>
-<div id="dataStep-2" style="display: none">
-				<!-- Dashboard Box -->
-	
+<div id="dataStep-2" style="display: none">	
 <div class="dashboard-box margin-top-0">
     <!-- Headline -->
     <div class="headline">
         <h3> Description </h3>
     </div>
-    <div class="content with-padding">
-    
+    <div class="content with-padding">    
             <div class="submit-field">
                 <label>Description about project</label>
                 <textarea  class="form-control" name="description" id="description"></textarea>
@@ -23,17 +20,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <label>Project attachment</label>                
                 <input type="file" name="fileinput" id="fileinput" multiple="true">
                 <div class="upload-area" id="uploadfile">
-                    <h4>Drag &amp; drop file here or <span class="text-site">click</span> to select file</h4>
+                    <p>Drag &amp; drop file here<br /> or<br /> <span class="text-site">click</span> to select file</p>
                 </div>
                 <div id="uploadfile_container"></div>
-            </div>
-        
+            </div>        
     </div>
     <div class="dashboard-box-footer">
         <button class="btn btn-secondary backbtnproject" data-step="2">Back</button>
         <button class="btn btn-site nextbtnproject" data-step="2">Next</button>
     </div>
 </div>
-				
-
 </div>
