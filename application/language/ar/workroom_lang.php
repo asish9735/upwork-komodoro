fc7f6b83bceb8c1e8e17d03e6f5aca9598f7c9d6
RajeshBera94 <81912980+RajeshBera94@users.noreply.github.com>
2021-04-21 11:38:27 +0530

    workroom_worklog

---
+++ application/language/ar/workroom_lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['workroom_worklog_start_date'] = 'تاريخ البدء:';
+$lang['workroom_worklog_end_date'] = 'تاريخ الانتهاء:';
+$lang['workroom_worklog_duration'] = 'مدة:';
+$lang['workroom_worklog_invoice'] = 'فاتورة';
+$lang['workroom_worklog_approved'] = 'وافق';
+$lang['workroom_worklog_rejected'] = 'مرفوض';
+$lang['workroom_worklog_reject'] = 'رفض';
+$lang['workroom_worklog_pending'] = 'قيد الانتظار';
+$lang['workroom_worklog_accept'] = 'قبول';
+$lang['workroom_worklog_got_rejected'] = 'تم رفض التقديم';
+$lang['workroom_worklog_reason'] = 'سبب:';
+$lang['workroom_worklog_no_record'] = 'لا أتذكر';
+
+
+
+?>
---
+++ application/language/en/workroom_lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['workroom_worklog_start_date'] = 'Start Date:';
+$lang['workroom_worklog_end_date'] = 'End Date:';
+$lang['workroom_worklog_duration'] = 'Duration:';
+$lang['workroom_worklog_invoice'] = 'Invoice';
+$lang['workroom_worklog_approved'] = 'Approved';
+$lang['workroom_worklog_rejected'] = 'Rejected';
+$lang['workroom_worklog_reject'] = 'Reject';
+$lang['workroom_worklog_pending'] = 'Pending';
+$lang['workroom_worklog_accept'] = 'Accept';
+$lang['workroom_worklog_got_rejected'] = 'Submission got rejected';
+$lang['workroom_worklog_reason'] = 'Reason:';
+$lang['workroom_worklog_no_record'] = 'No record';
+
+
+
+?>
