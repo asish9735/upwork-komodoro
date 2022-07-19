69d736d81c0469ddf32da4ab49234af690c15a88
Dev <originatesoft@gmail.com>
2022-07-19 23:30:52 +0530

    portfolio

--- application/views/default/proposals/post-step-6.php
+++ application/views/default/proposals/post-step-6.php
@@ -12,11 +12,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <div class="content with-padding">
         <div class="submit-field">
           <h5><b><?php echo __('postproposal_title','Title');?></b> <a href="javascript:void(0)" class="edit-proposal float-end btn btn-outline-secondary btn-circle" data-popup="1" data-tippy-placement="top" title="Edit title"><i class="icon-feather-edit-2"></i></a></h5>
-          <label><?php echo __('postproposal_proposal_name','Name of your proposal');?></label>
+          <label class="form-label"><?php echo __('postproposal_proposal_name','Name of your proposal');?></label>
           <p id="preview_title"></p>
-          <label><?php echo __('postproposal_proposal_category','Project category');?></label>
+          <label class="form-label"><?php echo __('postproposal_proposal_category','Project category');?></label>
           <p id="preview_category"></p>
-          <label><?php echo __('postproposal_proposal_skills','Tags');?></label>
+          <label class="form-label"><?php echo __('postproposal_proposal_skills','Tags');?></label>
           <div class="task-tags" id="preview_skills"></div>
         </div>
 		
@@ -31,10 +31,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
     <div class="content with-padding">
       <div class="submit-field mb-0">        
-        <label><?php echo __('postproposal_description','Description');?></label>
+        <label class="form-label"><?php echo __('postproposal_description','Description');?></label>
         <p id="preview_description"><?php echo __('postproposal_test_check','test check');?></p>
         <div id="preview_attachment_sec" style="display: none">
-        <label><?php echo __('postproposal_attachment','Attachment');?></label>
+        <label class="form-label"><?php echo __('postproposal_attachment','Attachment');?></label>
         <p id="preview_attachment"></p>
         </div>
       </div>
@@ -47,19 +47,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
     <div class="content with-padding">
       <div class="submit-field">        
-        <label><?php echo __('postproposal_proposal_type','Type of proposal');?></label>
+        <label class="form-label"><?php echo __('postproposal_proposal_type','Type of proposal');?></label>
         <p id="preview_proposalType"></p>
         <div id="preview_attachment_sec" style="display: none">
-        <label><?php echo __('postproposal_attachment','Attachment');?></label>
+        <label class="form-label"><?php echo __('postproposal_attachment','Attachment');?></label>
         <p id="preview_attachment"></p>
         </div>
       </div>
       <div class="submit-field">
-        <label><?php echo __('postproposal_screen_question','Screen question (optional)');?></label>
+        <label class="form-label"><?php echo __('postproposal_screen_question','Screen question (optional)');?></label>
         <div id="preview_question_sec" style="display: none"></div>
       </div>
       <div class="submit-field mb-0">  
-        <label><?php echo __('postproposal_cover_letter','Required cover letter');?></label>
+        <label class="form-label"><?php echo __('postproposal_cover_letter','Required cover letter');?></label>
         <p id="preview_is_cover_required"></p>
       </div>
     </div>
@@ -82,13 +82,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <div class="row">
         <div class="col-xl-6">
             <div class="submit-field">        
-                <label><?php echo __('postproposal_proposal_visibility','Project visibility');?></label>
+                <label class="form-label"><?php echo __('postproposal_proposal_visibility','Project visibility');?></label>
                 <p id="preview_proposalVisibility"></p>
             </div>
         </div>
         <div class="col-xl-6">
             <div class="submit-field"> 
-                <label><?php echo __('postproposal_freelancer_need','Freelancer need');?></label>
+                <label class="form-label"><?php echo __('postproposal_freelancer_need','Freelancer need');?></label>
                 <p id="preview_no_of_freelancer"></p>
       		</div>
         </div>
@@ -104,13 +104,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <div class="row">
         <div class="col-xl-6">
           <div class="submit-field">
-            <label><?php echo __('postproposal_h_fixed_price','Hourly or Fixed price');?></label>
+            <label class="form-label"><?php echo __('postproposal_h_fixed_price','Hourly or Fixed price');?></label>
             <p id="preview_proposalPaymentType"></p>
           </div>
         </div>
         <div class="col-xl-6">
           <div class="submit-field">
-            <label><?php echo __('postproposal_experience_level','Experience level');?></label>
+            <label class="form-label"><?php echo __('postproposal_experience_level','Experience level');?></label>
             <p id="preview_experience_level"></p>
           </div>
         </div>
@@ -118,13 +118,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
       <div class="row hourly_proposal_display" style="display: none">
         <div class="col-xl-6">
           <div class="submit-field">
-            <label><?php echo __('postproposal_proposal_duration','Project Duration');?></label>
+            <label class="form-label"><?php echo __('postproposal_proposal_duration','Project Duration');?></label>
             <p id="preview_hourly_duration"></p>
           </div>
         </div>
         <div class="col-xl-6">
           <div class="submit-field">
-            <label><?php echo __('postproposal_proposal_time','Project Time');?></label>
+            <label class="form-label"><?php echo __('postproposal_proposal_time','Project Time');?></label>
             <p id="preview_hourly_duration_time"></p>
           </div>
         </div>
